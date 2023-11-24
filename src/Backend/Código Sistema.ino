e5a99f4a79d5581a4796eb5cf36eb29024528575
deGuicS <131818444+deGuicS@users.noreply.github.com>
2023-11-24 01:17:49 -0300

    Update Código Sistema.ino

--- src/Backend/Código Sistema.ino
+++ src/Backend/Código Sistema.ino
@@ -1,53 +1,59 @@
-int portLedVermelho = 4;
-int portLedVerde = 0;
-int valSen = 0;
+#define BLYNK_TEMPLATE_ID "TMPL254KYQPHm"
+#define BLYNK_TEMPLATE_NAME "Hidroponia"
+#define BLYNK_AUTH_TOKEN "H91lrUurLrGp-uY0t8g2p0j9uQQSkcxn"
+
+#define BLYNK_PRINT Serial
+
+#define vSensor V0
+
+#include <ESP8266WiFi.h> // Incluindo a Biblioteca do Wifi do ESP8266
+#include <BlynkSimpleEsp8266.h> // Incluindo a Biblioteca do Blynk
+
+const char ssid[] = "gui"; // Login da Internet
+const char pass[] = "gui21042004"; // Senha da Internet
+
+int portLedVermelho = 4; // Definindo a Porta do LED Vermelho
+int portLedVerde = 0; // Definindo a Porta do LED Verde
+int valorSensor = 0;
 int portSen = A0;
-int portBot = 14;
-int estadoBot = 0;
 int portMot = 16;
 
-void setup()
-{
- 
+BlynkTimer timer; 
 
-  Serial.begin(9600);
+void setup() {
+  Serial1.begin(9600);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
   pinMode(portLedVermelho, OUTPUT);
   pinMode(portLedVerde, OUTPUT);
   pinMode(portSen, INPUT);
-  pinMode(portBot, INPUT);
   pinMode(portMot, OUTPUT);
 }
 
+void loop(){
 
-void loop()
-{
+Blynk.run();
+timer.run();
 
-   if (valSen > 300){
-    digitalWrite(portLedVerde, HIGH);
-    digitalWrite(portLedVermelho, LOW);
+valorSensor = analogRead(portSen);
+Serial.println(valorSensor); 
+Blynk.virtualWrite(vSensor, valorSensor);
 
-  }
-  
-  else {
-    digitalWrite(portLedVerde, LOW);   
-    digitalWrite(portLedVermelho, HIGH);
+if (valorSensor >= 300){
+  digitalWrite(portLedVerde, HIGH);
+  digitalWrite(portLedVermelho, LOW);
+}
 
-  }
-  
-  valSen = analogRead(portSen);
-   Serial.println(valSen);
- 
-  estadoBot = digitalRead(portBot);   
+else {
+  digitalWrite(portLedVerde, LOW);   
+  digitalWrite(portLedVermelho, HIGH);
+}
 
-  digitalWrite(portMot, HIGH);
-  Serial.println("Bomba Ligada");
-  Serial.println(valSen);
-  delay(10000);
-  digitalWrite(portMot, LOW);
-  Serial.println("Bomba Desligada");
-  Serial.println(valSen);
-  delay(8000);
-  
- 
+digitalWrite(portMot, HIGH);
+Serial.println("B Ligada");
+Blynk.logEvent("bomba_ligada", "A bomba d'água ligou, suas plantas estão sendo regadas!");
+delay(30000);
 
+digitalWrite(portMot, LOW);
+Serial.println("B Desligada");
+delay(30000);
 }
