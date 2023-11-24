c8e3598d67e92230fb14bfd1902320a5ccfc489c
deGuicS <131818444+deGuicS@users.noreply.github.com>
2023-11-24 01:48:49 -0300

    Add Comentarios no Código Sistema.ino
    Código Comentado

--- src/Backend/Código Sistema.ino
+++ src/Backend/Código Sistema.ino
@@ -1,59 +1,66 @@
+// Definição dos identificadores do Blynk
 #define BLYNK_TEMPLATE_ID "TMPL254KYQPHm"
 #define BLYNK_TEMPLATE_NAME "Hidroponia"
 #define BLYNK_AUTH_TOKEN "H91lrUurLrGp-uY0t8g2p0j9uQQSkcxn"
 
+// Configuração para imprimir mensagens do Blynk no console Serial
 #define BLYNK_PRINT Serial
 
+// Identificador virtual do sensor no Blynk
 #define vSensor V0
 
-#include <ESP8266WiFi.h> // Incluindo a Biblioteca do Wifi do ESP8266
-#include <BlynkSimpleEsp8266.h> // Incluindo a Biblioteca do Blynk
+// Inclusão das bibliotecas necessárias para WiFi e Blynk
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
-const char ssid[] = "gui"; // Login da Internet
-const char pass[] = "gui21042004"; // Senha da Internet
+// Configuração do nome e senha da rede WiFi
+const char ssid[] = "gui";
+const char pass[] = "gui21042004";
 
-int portLedVermelho = 4; // Definindo a Porta do LED Vermelho
-int portLedVerde = 0; // Definindo a Porta do LED Verde
+// Definição das portas para LED vermelho, LED verde, sensor, e motor
+int portLedVermelho = 4;
+int portLedVerde = 0;
 int valorSensor = 0;
-int portSen = A0;
-int portMot = 16;
+int portSen = A0; // Porta analógica para o sensor
+int portMot = 16; // Porta para o motor
 
-BlynkTimer timer; 
+BlynkTimer timer; // Criação do timer do Blynk para eventos
 
 void setup() {
-  Serial1.begin(9600);
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  Serial1.begin(9600); // Inicialização da comunicação serial
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80); // Inicialização do Blynk
   pinMode(portLedVermelho, OUTPUT);
   pinMode(portLedVerde, OUTPUT);
-  pinMode(portSen, INPUT);
-  pinMode(portMot, OUTPUT);
+  pinMode(portSen, INPUT); // Configuração da porta do sensor como entrada
+  pinMode(portMot, OUTPUT); // Configuração da porta do motor como saída
 }
 
 void loop(){
+  Blynk.run(); // Execução do loop do Blynk
+  timer.run(); // Execução do timer do Blynk
 
-Blynk.run();
-timer.run();
+  valorSensor = analogRead(portSen); // Leitura do valor do sensor analógico
+  Serial.println(valorSensor);
+  Blynk.virtualWrite(vSensor, valorSensor); // Atualização do valor do sensor no Blynk
 
-valorSensor = analogRead(portSen);
-Serial.println(valorSensor); 
-Blynk.virtualWrite(vSensor, valorSensor);
+  // Lógica para controle dos LEDs com base no valor do sensor
+  if (valorSensor >= 300){
+    digitalWrite(portLedVerde, HIGH);
+    digitalWrite(portLedVermelho, LOW);
+  } else {
+    digitalWrite(portLedVerde, LOW);
+    digitalWrite(portLedVermelho, HIGH);
+  }
 
-if (valorSensor >= 300){
-  digitalWrite(portLedVerde, HIGH);
-  digitalWrite(portLedVermelho, LOW);
+  // Aciona o motor, registra um evento no Blynk e aguarda por 30 segundos
+  digitalWrite(portMot, HIGH);
+  Serial.println("B Ligada");
+  Blynk.logEvent("bomba_ligada", "A bomba d'água ligou, suas plantas estão sendo regadas!");
+  delay(30000);
+
+  // Desliga o motor, registra um evento no Blynk e aguarda por mais 30 segundos
+  digitalWrite(portMot, LOW);
+  Serial.println("B Desligada");
+  delay(30000);
 }
 
-else {
-  digitalWrite(portLedVerde, LOW);   
-  digitalWrite(portLedVermelho, HIGH);
-}
-
-digitalWrite(portMot, HIGH);
-Serial.println("B Ligada");
-Blynk.logEvent("bomba_ligada", "A bomba d'água ligou, suas plantas estão sendo regadas!");
-delay(30000);
-
-digitalWrite(portMot, LOW);
-Serial.println("B Desligada");
-delay(30000);
-}
